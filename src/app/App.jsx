40a242797cf4e7f27aa3bdb734985acddc43b17b
Aaron Hermann <a.hermann.1@gmx.de>
2025-06-25 15:26:21 +0200

    Fixed dark mode beeing sometimes to dark again

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,26 @@
 // src/App.jsx
-import { useMemo } from 'react'
-import { CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material'
+import { useEffect, useMemo, useState } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { getTheme } from './theme/theme'
 import { TimetableProvider } from '../shared/context/TimetableContext'
 import MainLayout from './layouts/MainLayout'
 
 function App() {
-  const prefersDark = useMediaQuery('(prefers-color-scheme: dark)')
-  const theme = useMemo(() => getTheme(prefersDark ? 'dark' : 'light'), [prefersDark])
+  const [mode, setMode] = useState('light')
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+    const handleChange = () => {
+      setMode(mediaQuery.matches ? 'dark' : 'light')
+    }
+
+    handleChange() // set initially
+    mediaQuery.addEventListener('change', handleChange)
+    return () => mediaQuery.removeEventListener('change', handleChange)
+  }, [])
+
+  const theme = useMemo(() => getTheme(mode), [mode])
 
   return (
     <TimetableProvider>
